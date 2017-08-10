60a8e1d357a10a9f8cadc2e5fb622db6d323c05e
Robert Monden <dev@robertmonden.com>
2017-08-10 11:56:25 +0200

    Initial commit

---
+++ test/TestHawk.php
@@ -0,0 +1,5 @@
+<?php
+
+class TestHawk extends Hawk
+{
+}
---
+++ test/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+include 'Hawk;
+
+if ( ! class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
